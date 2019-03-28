f226a9a64b4a561030d8dd2652ab50dbe6b2dc25
tjxuning <Xuning696397@>
2019-03-28 22:41:36 +0800

    栈

--- damien2-java-algorithm/src/test/java/com/example/damien2javaalgorithm/Damien2JavaAlgorithmApplicationTests.java
+++ damien2-java-algorithm/src/test/java/com/example/damien2javaalgorithm/Damien2JavaAlgorithmApplicationTests.java
@@ -23,7 +23,7 @@ public class Damien2JavaAlgorithmApplicationTests {
 
     }
 
-    public void hystrix(){
+    public void hystrixhystrixhystrixhystrixhystrixhystrixhystrixhystrixhystrix(){
 		System.out.println("111111");
 
 
