3794c157f9e7ad1ffca067dad38e0860dd1956f8
tjxuning <Xuning696397@>
2019-04-02 14:28:48 +0800

    java 代码

---
+++ damien2-java/src/main/java/com/example/damien2java/Damien2JavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.damien2java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Damien2JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Damien2JavaApplication.class, args);
+	}
+
+}
